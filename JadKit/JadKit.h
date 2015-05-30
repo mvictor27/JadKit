22101fa878c4c594a9f77a7b1f7c3b34318442c8
Jad Osseiran <jad6@icloud.com>
2015-05-29 20:06:41 -0700

    Initial Commit

---
+++ JadKit/JadKit.h
@@ -0,0 +1,19 @@
+//
+//  JadKit.h
+//  JadKit
+//
+//  Created by Jad Osseiran on 29/05/2015.
+//  Copyright (c) 2015 Jad Osseiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JadKit.
+FOUNDATION_EXPORT double JadKitVersionNumber;
+
+//! Project version string for JadKit.
+FOUNDATION_EXPORT const unsigned char JadKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JadKit/PublicHeader.h>
+
+
